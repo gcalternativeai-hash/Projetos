13b86a8953b5f5568402bbc06b392dac1b59990c
Kayzzo33 <teamredzone.mk@gmail.com>
2025-10-01 18:36:20 -0300

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,7 @@ export function Header() {
             <img
               src="/images/OnzyLogo.png"
               alt="Sua Logo"
-              className="h-40 sm:h-10 w-auto"
+              className="h--40 sm:h-10 w-auto"
             />
           </div>
 
